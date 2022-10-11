297215c71b86fd6ef02457eeb668ed160f96dbe8
rakeshwasnik <rbwasnik99@gmail.com>
2022-10-11 08:59:04 +0000

    Develop HTML Structure

---
+++ src/lib/todo-item.svelte
@@ -0,0 +1,15 @@
+<div>
+    <form action="" method="" >
+        <input type="hidden" name="done" value="" />
+        <button aria-label="Mark Done/Not Done" >Done/Not Done</button>
+    </form>
+
+    <form action="" method="">
+        <input type="text" />
+        <button aria-label="Save ToDo">Save</button>
+    </form>
+
+    <form action="" method="">
+        <button aria-label="Delete ToDo"></button>
+    </form>
+</div>
